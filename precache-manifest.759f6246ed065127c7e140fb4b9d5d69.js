33411cb9c7257164c72f15d64acc7f13b9f56564
Loraine <38534852+lorifranke@users.noreply.github.com>
2020-09-29 13:45:27 -0400

    Updates

---
+++ precache-manifest.759f6246ed065127c7e140fb4b9d5d69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6459319e7a6ed4416622746c121fe909",
+    "url": "/FiberStars/index.html"
+  },
+  {
+    "revision": "3ccf1a7362fe7e12e409",
+    "url": "/FiberStars/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b2cc6137cd78519b27ce",
+    "url": "/FiberStars/static/css/main.aebde75a.chunk.css"
+  },
+  {
+    "revision": "3ccf1a7362fe7e12e409",
+    "url": "/FiberStars/static/js/2.6df4354d.chunk.js"
+  },
+  {
+    "revision": "27b1cb995562af413ecdb85331cc16bd",
+    "url": "/FiberStars/static/js/2.6df4354d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cc6137cd78519b27ce",
+    "url": "/FiberStars/static/js/main.b379160d.chunk.js"
+  },
+  {
+    "revision": "905826094f5840288791",
+    "url": "/FiberStars/static/js/runtime-main.0b0edef7.js"
+  }
+]);
